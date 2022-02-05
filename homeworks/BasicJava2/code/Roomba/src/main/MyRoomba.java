0aca3f20600094fa2daa15c8d05034493af6622b
Mark Floryan <floryan@floryan.local>
2022-02-04 20:56:52 -0500

    updating default Roomba code

--- homeworks/BasicJava2/code/Roomba/src/main/MyRoomba.java
+++ homeworks/BasicJava2/code/Roomba/src/main/MyRoomba.java
@@ -5,8 +5,8 @@ import world.Roomba;
 
 public class MyRoomba extends Roomba{
 	
-	private boolean turning = false;
-	private Direction newDir = Direction.NORTH;
+	/*You can put variables here if it will be helpful*/
+	/*Examples: Roomba is currently turning, Roomba is trying to get to some new direction, etc. */
 
 	public MyRoomba(int x, int y, int radius) {
 		super(x, y, radius);
@@ -16,17 +16,11 @@ public class MyRoomba extends Roomba{
 	public Move makeMove() {
 		/*TODO: Make this method better. Here's an example Roomba that always turns a random direction*/
 		
-		if(this.turning && this.getDirection() == newDir) {
-			this.turning = false;
-		}
-		
-		if(this.frontBumper && !this.turning) {
-			turning = true;
-			newDir = Direction.values()[(int)(Math.random()*Direction.values().length)];
+		//if we bump into something, turn
+		if(this.frontBumper) 
 			return Move.TURNCLOCKWISE;
-		}
 		
-		if(this.turning) { return Move.TURNCLOCKWISE; }
+		//otherwise just move forward
 		return Move.FORWARD;
 	}
 	
